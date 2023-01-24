42aea8af458baf56c258cdb8e7d9d211f5424e5b
hechmi dammak <hec.hmi@hotmail.fr>
2023-01-24 21:41:28 +0100

    fix deselect all

--- lib/views/recipe_categories/recipe_categories_controller.dart
+++ lib/views/recipe_categories/recipe_categories_controller.dart
@@ -43,7 +43,7 @@ class RecipeCategoriesController extends ControllerDecorator {
     for (var recipeCategory in recipeCategories) {
       recipeCategory.selected = value;
     }
-    setSelectAllValue(value: value, callChild: false);
+    super.setSelectAllValue(value: value, callChild: false);
   }
 
   @override
